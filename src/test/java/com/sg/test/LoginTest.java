3903563347fa17c07154dba2494be0f72e9f33b5
AbiramiKarunanithi <abiramikarunanithi@gmail.com>
2021-08-14 14:15:27 +0530

    logintest - validcredentialtest - dataprovider

--- src/test/java/com/sg/test/LoginTest.java
+++ src/test/java/com/sg/test/LoginTest.java
@@ -1,6 +1,7 @@
 package com.sg.test;
 	
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.sg.base.WebDriverWrapper;
@@ -13,8 +14,30 @@ public class LoginTest extends WebDriverWrapper {
 	//admin,pass,English (Indian),OpenEMR
 	//physician,physician,English (Indian),OpenEMR
 	//accountant,accountant,English (Indian),OpenEMR
-	//
-	@Test
+	@DataProvider
+	public Object[][] validCredentialData()
+	{
+		Object[][] main=new Object[3][4];
+		
+		main[0][0]="admin";
+		main[0][1]="pass";
+		main[0][2]="English (Indian)";
+		main[0][3]="OpenEMR";
+		
+		main[1][0]="physician";
+		main[1][1]="physician";
+		main[1][2]="English (Indian)";
+		main[1][3]="OpenEMR";
+		
+		main[2][0]="accountant";
+		main[2][1]="accountant";
+		main[2][2]="English (Indian)";
+		main[2][3]="OpenEMR";
+		
+		return main;
+	}
+	
+	@Test(dataProvider = "validCredentialData")
 	public void validCredentialTest(String username,String password,String language,String expectedValue) {
 
 		LoginPage login=new LoginPage(driver);
