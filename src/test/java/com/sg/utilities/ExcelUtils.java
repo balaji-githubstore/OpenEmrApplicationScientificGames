db9b46706749e5661d87f3b36249d0dc6292421b
AbiramiKarunanithi <abiramikarunanithi@gmail.com>
2021-08-14 16:00:34 +0530

    excelutils added - sheet to two dim object array

---
+++ src/test/java/com/sg/utilities/ExcelUtils.java
@@ -0,0 +1,39 @@
+package com.sg.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+
+	public static Object[][] getSheetToObjectArray(String fileDetail, String sheetName) throws IOException {
+		FileInputStream file = new FileInputStream(fileDetail); // location
+
+		XSSFWorkbook book = new XSSFWorkbook(file); // open
+
+		XSSFSheet sheet = book.getSheet(sheetName); // sheet
+
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		int cellCount = sheet.getRow(1).getPhysicalNumberOfCells();
+
+		Object[][] main = new Object[rowCount - 1][cellCount];
+
+		DataFormatter format = new DataFormatter();
+
+		for (int r = 1; r < rowCount; r++) {
+			for (int c = 0; c < cellCount; c++) {
+				XSSFRow row = sheet.getRow(r);
+				XSSFCell cell = row.getCell(c);
+				main[r - 1][c] = format.formatCellValue(cell);
+			}
+		}
+
+		return main;
+	}
+
+}
